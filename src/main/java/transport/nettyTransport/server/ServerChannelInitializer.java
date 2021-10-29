c20c93420532b2978044d287896427159011c1d6
用MercerJR <973570588@qq.com>
2021-10-29 22:36:18 +0800

    新新建分层，添ått
    æ·transport层添加服务端代码

---
+++ src/main/java/transport/nettyTransport/server/ServerChannelInitializer.java
@@ -0,0 +1,21 @@
+package transport.nettyTransport.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+
+/**
+ * @Author: Mercer JR
+ * @Date: 2021/10/29 22:19
+ */
+public class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("decoder",new ProtobufDecoder(null));
+        pipeline.addLast("encoder",new ProtobufEncoder());
+        pipeline.addLast(new ServerHandler());
+    }
+}
---
+++ src/main/java/transport/nettyTransport/server/ServerStart.java
@@ -0,0 +1,33 @@
+package transport.nettyTransport.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @Author: Mercer JR
+ * @Date: 2021/10/29 22:18
+ */
+public class ServerStart {
+
+    public static void startServer(String hostname,int port){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ServerChannelInitializer());
+            ChannelFuture channelFuture = serverBootstrap.bind(hostname, port).sync();
+            channelFuture.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
